99d46832b2ef6ab1096c2de8aac626f321d7d386
ibsen <ibrahimdeiaa@gmail.com>
2023-03-21 23:18:19 +0100

    added determinstic and non determnistic cases

--- Graph.fs
+++ Graph.fs
@@ -11,9 +11,12 @@ open AST
 
 
 
-type Input = { 
-    determinism: Determinism
-    }
+type Determinism = Deterministic of string | NonDeterministic of string
+
+type Input =
+    { Case : string }
+
+
 
 type Output = { dot: string }
 
@@ -33,30 +36,60 @@ let getNextNodeName() =
 
 
 
+
+
+let rec edgesDeterminstic(ast,qS,qF) = 
+    match ast with 
+        | Skip -> [{source = qS ; label = S; target = qF}] 
+        | Assign(_,_) -> [{source = qS; label = S ; target = qF}]
+        | C(c1,c2) -> 
+                            let q = getNextNodeName()
+                            edgesDeterminstic(c1,qS,q) @ edgesDeterminstic(c2,q,qF)
+        | Do gc -> edgesGCD(gc,qS,qF) 
+        | If gc -> edgesGCD(gc,qS,qF)
+
+and edgesGCD(ast: gcexp,qS,qF) = 
+        match ast with
+                | Then(b,c) ->  
+                            let q = getNextNodeName()
+                            [{source =qS ; label = B(And(b,Not(b))) ; target=q}] @ edgesDeterminstic(c,qS,qF)
+                | GC (gc1,gc2) -> edgesGCD(gc1,qS,qF) @ edgesGCD(gc2,qS,qF)
+
+
+
 let rec edgesC(ast: cexp, qS:Node,qF:Node): List<Edge>  = 
     match ast with
         | Skip ->  [{source =qS ; label = S ; target=qF}]
         | Assign(_,_) -> [{source = qS; label = K ast; target =qF}]
         | C (c1,c2) -> 
-            let q = getNextNodeName()
-            edgesC(c1, qS, q) @ edgesC(c2,q,qF)
+             let q = getNextNodeName()
+             edgesC(c1, qS, q) @ edgesC(c2,q,qF)
         | If gc ->   edgesGC(gc,qS,qF) 
-        | Do gc ->  edgesGC(gc,qS,qF) 
+        | Do gc ->  edgesGC(gc,qS,qF) //@ doneGC(gc,qS,qF)
+
         
 
 and edgesGC (ast, qS, qF) = 
     match ast with 
         | Then(b,c) -> 
-                let q = getNextNodeName() 
-                [{source =qS ; label = B b ; target=q}]  @ edgesC(c,q,qF) @  [{source =qS ; label = B (Not b); target=qF}]
+                let q1 = getNextNodeName() 
+                [{source =qS ; label = B b ; target=q1}]  @ edgesC(c,q1,qS) @  [{source =qS ; label = B (Not b); target=qF}]
         | GC(gc1,gc2) -> let q1 = getNextNodeName()
                          let q2 = getNextNodeName()
-                         edgesGC(gc1,qS,q1) @ edgesGC(gc2,q1,qF)
+                         edgesGC(gc1,qS,q1) @ edgesGC(gc2,q2,q1)
 
 
 
-let ast2pg(ast): List<Edge>  = 
-    edgesC(ast,"q0","qf")
+
+
+
+let ast2pg(ast,input): List<Edge>  = 
+        match input with
+            |{Case = "Determinstic"} -> edgesDeterminstic(ast,"q0","qf")
+            |{Case = "NonDeterministic"} ->  edgesC(ast,"q0","qf")
+            |_ -> failwith "Not a valid case"
+
+   
 
 
 
@@ -85,7 +118,7 @@ let pg2dot (pg : List<Edge>): string =
 let analysis (src: string) (input: Input) : Output =
     match parse Parser.startGCL (src) with
         | Ok ast ->
-            let pg = ast2pg(ast)
+            let pg = ast2pg(ast,input)
           //  Console.Error.WriteLine("> {0}",pg)
             let dotstring = pg2dot(pg)
             { dot = dotstring }
